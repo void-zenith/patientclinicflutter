703ced80cac79142847d30cb085101d6e981dafd
void-zenith <zenithrajbhandari@gmail.com>
2024-03-11 19:07:18 -0400

    made models

---
+++ lib/models/Patient.dart
@@ -0,0 +1,11 @@
+class Patient {
+  final String fullName;
+  final String email;
+  final String address;
+  final String number;
+  final String gender;
+  final DateTime dob;
+
+  Patient(this.fullName, this.email, this.address, this.number, this.gender,
+      this.dob);
+}
